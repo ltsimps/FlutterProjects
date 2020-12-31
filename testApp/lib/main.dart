3a9b7d09808e2ba3042225e30a17ce9f2f7718a5
ltsimps <ltsimpso@me.com>
2020-12-30 20:34:19 -0500

    Added test streak feature

--- testApp/lib/main.dart
+++ testApp/lib/main.dart
@@ -40,7 +40,9 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Card(
               child: Text("Media Place Holder"),
-            )
+            ),
+            ChartLine(rate: 0.5, title: "Streak"),
+            ChartLine(rate: 0.5, title: "pushups"),
           ],
         ),
       ),
@@ -50,5 +52,66 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class ChartLine extends StatelessWidget {
+  const ChartLine({
+    Key key,
+    @required this.rate,
+    @required this.title,
+    this.number,
+  })  : assert(title != null),
+        assert(rate != null),
+        assert(rate > 0),
+        assert(rate <= 1),
+        super(key: key);
+
+  final double rate;
+  final String title;
+  final int number;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      final lineWidget = constraints.maxWidth * rate;
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              constraints: BoxConstraints(minWidth: lineWidget),
+              child: IntrinsicWidth(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    if (number != null)
+                      Text(
+                        number.toString(),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              height: 60,
+              width: lineWidget,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
