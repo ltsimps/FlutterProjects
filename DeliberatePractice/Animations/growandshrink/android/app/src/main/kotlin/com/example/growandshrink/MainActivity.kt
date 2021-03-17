c421c571620195df1d3dd3833612de1ee1e14930
ltsimps <ltsimpso@me.com>
2021-03-17 13:15:26 -0400

    updated projects

---
+++ DeliberatePractice/Animations/growandshrink/android/app/src/main/kotlin/com/example/growandshrink/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.growandshrink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ DeliberatePractice/Animations/growandshrink/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _size = 100;
+
+  void _changeShape() {
+    setState(() {
+      _size = _size == 100 ? 200 : 100;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title!),
+      ),
+      body: Center(
+        child: Container(width: _size, height: _size, color: Colors.green),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _changeShape,
+        tooltip: 'Increment',
+        child: Icon(Icons.play_arrow),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
