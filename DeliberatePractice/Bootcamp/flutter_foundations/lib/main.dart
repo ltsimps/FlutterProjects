d87f968e93d112be70228b094513e25cc34aec23
ltsimps <ltsimpso@me.com>
2021-06-05 16:45:17 -0400

    working on repo viwer

--- DeliberatePractice/Bootcamp/flutter_foundations/lib/main.dart
+++ DeliberatePractice/Bootcamp/flutter_foundations/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_foundations/range_selector_page.dart';
 import 'package:flutter_foundations/ranomizer_change_notifier.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart'; 
 
 void main() {
   runApp(AppWidget());
@@ -24,3 +24,4 @@ class AppWidget extends StatelessWidget {
     );
   }
 }
+
